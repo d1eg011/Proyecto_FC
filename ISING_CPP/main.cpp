03df73aaf27b3dd3a66acf867c08bc13cdc3a21a
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-07-01 09:15:06 -0600

    test file usual changes

--- ISING_CPP/main.cpp
+++ ISING_CPP/main.cpp
@@ -7,7 +7,7 @@
 int main(){
   unsigned int N = 50;
   Spin lattice(N);
-  lattice.configuration_update(0.1, 1.0, 0.0, 2500).print_lattice();
+  lattice.configuration_update(0.1, 1.0, 0.0, 10000).print_lattice();
   std::cout << "Energy: " << lattice.get_energy() << " " <<
                "Magnetization: " << lattice.get_magnetization() << std::endl;
 
