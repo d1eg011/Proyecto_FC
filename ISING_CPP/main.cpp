622e91339cf496cfaaa6dbf7b4bdcf830ec5b455
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-07-04 13:49:29 -0600

    Usual changes to test code (it worked)

--- ISING_CPP/main.cpp
+++ ISING_CPP/main.cpp
@@ -3,9 +3,9 @@
 int main(){
   unsigned int N = 20;
   Spin lattice(N);
-  lattice.configuration_update(0.0, 1.0, 0.0, 10000).print_lattice();
-  
-  lattice.configuration_update(2.0, 1.0, 0.0, 1000000).print_lattice();
+  lattice.configuration_update(0.0, 1.0, 0.0, 10000);
+
+  lattice.configuration_update(2.0, 1.0, 0.0, 2000000);
 
   std::cout << "Energy: " << lattice.calcEnergy() << " " <<
                "Magnetization: " << lattice.calcMagnetization() << std::endl;
