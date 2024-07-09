08c9c948aa478303e05728802e0c2727523a69d1
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-07-09 00:15:03 -0600

    Energy and Magnetization run and plotting added

--- ISING_CPP_SERIAL/run_ising.cpp
+++ ISING_CPP_SERIAL/run_ising.cpp
@@ -1,6 +1,6 @@
 #include "spin.hpp"
 
-void plot(std::vector<double>& x, std::vector<double>& y, std::string file_name, std::string Title, std::string Xlabel, std::string Ylabel, std::string Legend) {
+void plot(std::vector<double>& x, std::vector<double>& y, std::string file_name, std::string Title, std::string Xlabel, std::string Ylabel, std::string color, std::string Legend) {
     Gnuplot gp;
 
     // Set the terminal to pdfcairo for direct PDF output
@@ -14,7 +14,7 @@ void plot(std::vector<double>& x, std::vector<double>& y, std::string file_name,
     gp << "set ytics scale 2\n";
     gp << "set xtics scale 2\n";
 
-    gp << "plot '-' with points pointtype 7 pointsize 0.75 linecolor rgb 'blue' title '" << Legend << "'\n";
+    gp << "plot '-' with points pointtype 7 pointsize 0.75 linecolor rgb" << color << "title '" << Legend << "'\n";
     gp.send1d(boost::make_tuple(x, y));
 
     std::cout << "Plot saved to " << file_name << ".pdf" << std::endl;
@@ -38,27 +38,37 @@ int main(){
   float H = 0.0;
   unsigned int size = 50;
   unsigned int its = 500000;
-  unsigned int steps = 100.0;
-  double beta_min = 0.02;
-  double beta_max = 1.2;
+  unsigned int steps = 50.0;
+  double T_min = 0.01;
+  double T_max = 4.0;
 
-  std::vector<double> beta(steps);
+  std::vector<double> T(steps , 0.0);
   std::vector<double> mean_energy( steps, 0.0);
   std::vector<double> mean_magnetization(steps, 0.0);
-  
+  std::vector<double> meansq_energy( steps, 0.0 );
+ //std::vector<double> sqmean_energy( steps, 0.0 );
+ //std::vector<double> specificHeat( steps, 0.0 );
+
   Spin lattice(size);
-  beta = sampling(beta_min,beta_max, steps);
+  T = sampling(T_min,T_max, steps);
 
   for (unsigned i = 0; i < steps; ++i){
-    lattice.configuration_update(beta[i], J, H, its);
+    lattice.configuration_update(1/T[i], J, H, its);
     
-    mean_magnetization[i] = static_cast<double>(abs(lattice.calcMagnetization()))/ (size*size);
+    mean_energy[i] = lattice.getEnergy() / (4.0*its);
+    mean_magnetization[i] = std::abs(lattice.getMagnetization()) / (2.0*its);
+    meansq_energy[i] = std::pow(lattice.getEnergy(), 2) / (4.0*its);
+
 
     lattice.configuration_reset();
   }
   
+  plot(T, mean_energy, "Energy_plot", "Normalized mean energy per spin", "Temperature (kT)", "Energy", "'red'" ,"mean energy per spin");
 
-  plot(beta, mean_magnetization, "Magnetization_plot", "Normalized mean Magnetization", "Beta", "mean energy", "magnetization per spin");
+  plot(T, mean_magnetization, "Magnetization_plot", "Normalized Magnetization per spin", "Temperature (kT)", "Magnetization", "'blue'" ,"mean magnetization per spin");
+
+  //plot(T, specificHeat, "SpecHeat_plot", "Specific Heat", "Beta", "specific heat", "specific heat");
+
 
   return 0;
 }
