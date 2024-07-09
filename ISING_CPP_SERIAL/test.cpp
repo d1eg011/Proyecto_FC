dbce99a4fd3df115d597411bebd2c58b2547e839
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-07-09 01:18:12 -0600

    unimportant tests on class methods

--- ISING_CPP_SERIAL/test.cpp
+++ ISING_CPP_SERIAL/test.cpp
@@ -3,9 +3,10 @@
 int main(){
   unsigned int N = 20;
   Spin lattice(N);
-  lattice.configuration_update(0.0, 1.0, 0.0, 100000);
+  lattice.configuration_update(0.5, 1.0, 0.0, 100000);
 
-  std::cout << lattice.getMagnetization()/(2*100000.0)  << std::endl;
+
+  std::cout << lattice.getSqenergy()/(4.0*100000.0) << "  " << std::pow(lattice.getEnergy()/(100000.0),2)/4  << std::endl;
   //lattice.lattice_cmap();
 
 
