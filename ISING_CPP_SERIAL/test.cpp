67640b5f58ca611467513f3abfa0b40c367daff0
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-07-09 00:16:19 -0600

    tests on class

--- ISING_CPP_SERIAL/test.cpp
+++ ISING_CPP_SERIAL/test.cpp
@@ -1,16 +1,14 @@
 #include "spin.hpp"
 
 int main(){
-  unsigned int N = 500;
+  unsigned int N = 20;
   Spin lattice(N);
-  lattice.configuration_update(0.0, 1.0, 0.0, 5000000);
+  lattice.configuration_update(0.0, 1.0, 0.0, 100000);
 
-  lattice.lattice_cmap();
+  std::cout << lattice.getMagnetization()/(2*100000.0)  << std::endl;
+  //lattice.lattice_cmap();
 
-  //lattice.configuration_update(2.0, 1.0, 0.0, 2000000);
 
-  std::cout << "Energy: " << lattice.calcEnergy() << " " <<
-               "Magnetization: " << lattice.calcMagnetization() << std::endl;
 
   return 0;
 }
